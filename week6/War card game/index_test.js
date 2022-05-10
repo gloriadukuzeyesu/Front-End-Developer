2cf3687cd5774f275787d004ec7f0be3c16729e9
Gloria Dukuzeyesu <gloriadukuzeyesu@ianr-c02tclych3qd.lan>
2022-05-10 12:10:22 -0500

    card game project

--- week6/War card game/index_test.js
+++ week6/War card game/index_test.js
@@ -5,11 +5,10 @@ describe('MyFunction' , function(){
     describe('#createDeck' , function (){
         it ('this function should create a deck of cards', function(){
             var x= createCards();
-            var suit=['hearts', 'spade','diamond','club'];
-            var value=[ 2,3,4,5,6,7,8,9,10,11,12,13,14];
-            expect (x).to.equal (52.[new Card( suit, value)])
-            ;   
-        });
+            /* var suit=['hearts', 'spade','diamond','club'];
+            var value=[ 2,3,4,5,6,7,8,9,10,11,12,13,14]; */
+            expect(x).to.have.lengthOf(52);
+            expect(x.every((card) => { card instanceof Card })).to.be.false
 
         it('should throw an error if the function has a parameter passed to it', function() {
             expect(function() {
@@ -18,5 +17,7 @@ describe('MyFunction' , function(){
         });
     });
 });
+});
+
 
 
